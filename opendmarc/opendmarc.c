92f073490bb99d594067ebc8dd42afab57b2dd77
José Borges Ferreira <underspell@gmail.com>
2016-05-31 20:19:45 +0100

    Fix for ticket 158 - https://sourceforge.net/p/opendmarc/tickets/158/

--- opendmarc/opendmarc.c
+++ opendmarc/opendmarc.c
@@ -1696,7 +1696,10 @@ mlfi_connect(SMFICTX *ctx, char *host, _SOCK_ADDR *ip)
 
 	if (dmarcf_checkhost(host, ignore) ||
 	    (ip != NULL && dmarcf_checkip(ip, ignore)))
+	{
+		syslog(LOG_INFO, "ignoring connection from %s", host);
 		return SMFIS_ACCEPT;
+	}
 
 	/* copy hostname and IP information to a connection context */
 	cc = dmarcf_getpriv(ctx);
